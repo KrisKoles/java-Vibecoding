39cf01c32dc1429c111fccb1064a2ea56ccab07c
kriskoles <kris.demchenko2016@yandex.ru>
2026-06-23 14:11:15 +0300

    feat: добавлен класс Main

--- src/main/java/ru/Homework10/TaskTracker/Main.java
+++ src/main/java/ru/Homework10/TaskTracker/Main.java
@@ -1,4 +1,28 @@
 package ru.Homework10.TaskTracker;
 
 public class Main {
+    public static void main(String[] args){
+        TaskTracker tracker = new TaskTracker();
+
+        // Добавляем задачи
+        tracker.addTask("Купить хлеб");
+        tracker.addTask("Купить молоко");
+        tracker.addTask("Поиграть");
+        tracker.addTask("Поспать");
+
+        // Выводим список
+        tracker.printAllTasks();
+
+        // Отмечаем задачу
+        tracker.markTaskAsCompleted("Купить хлеб");
+
+        // Снова выводим
+        tracker.printAllTasks();
+
+        // Статистика
+        tracker.printStatistics();
+
+        // Поиск
+        tracker.findTaskByTitle("Купить хлеб");
+    }
 }
