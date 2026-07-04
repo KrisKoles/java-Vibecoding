2d088760f7e13854906be8c3d4dff4f37b1a375a
kriskoles <kris.demchenko2016@yandex.ru>
2026-07-04 21:29:08 +0300

    Написана логика класса BaggageDropDesk

---
+++ src/main/java/ru/Homework12/BaggageDropDesk.java
@@ -0,0 +1,33 @@
+package ru.Homework12;
+
+import ru.Homework12.exceptions.*;
+
+import java.util.Arrays;
+
+
+public class BaggageDropDesk {
+    private String[] availableFlights;
+    private static final int MAX_WEIGHT = 23;
+
+    public BaggageDropDesk(){
+        this.availableFlights = new String[]{ "SU-123", "TK-777", "KC-909", "AE-404" };
+    }
+
+    //сдача багажа
+    public BaggageTicket baggageCheckIn(String passengersName, String flightNumber, int baggageWeight)
+            throws FlightNotFoundException, OverweightBaggageException, BaggageTagPrintException{
+        if(passengersName == null || passengersName.trim().isEmpty()){
+            throw new InvalidPassengerNameException("Имя пассажира не может быть пустым.");
+        }if(baggageWeight <= 0){
+            throw new InvalidBaggageWeightException("Вес багажа меньше или равен нулю.");
+        }if(!Arrays.asList(availableFlights).contains(flightNumber)){
+            throw new FlightNotFoundException("Рейс " + flightNumber + " не найден.");
+        }if(baggageWeight > MAX_WEIGHT){
+            throw new OverweightBaggageException("Вес багажа " + baggageWeight + " кг превышает лимит " + MAX_WEIGHT + " кг.");
+        }if("AE-404".equals(flightNumber)){
+            throw new BaggageTagPrintException("Не удалось напечатать багажную бирку для рейса " + flightNumber + ".");
+        }
+        System.out.println("Багаж успешно зарегистрирован для " + passengersName + " Необходимости уплатить налог за слишком правильные введенные данные");
+        return new BaggageTicket(passengersName, flightNumber, baggageWeight);
+    }
+}
