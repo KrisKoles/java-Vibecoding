5f8f7de47387c5ba45d5fe17f118041d0b5f58fc
kriskoles <kris.demchenko2016@yandex.ru>
2026-07-04 19:04:07 +0300

    Добавлена логика класса BaggageTicket

---
+++ src/main/java/ru/Homework12/App.java
@@ -0,0 +1,8 @@
+package ru.Homework12;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] flights){
+
+    }
+}
--- src/main/java/ru/Homework12/BaggageTicket.java
+++ src/main/java/ru/Homework12/BaggageTicket.java
@@ -1,4 +1,34 @@
 package ru.Homework12;
 
 public class BaggageTicket {
+    private String passengersName;
+    private String flightNumber;
+    private int baggageWeight;
+
+    public BaggageTicket(String passengersName, String flightNumber, int baggageWeight){
+        this.passengersName = passengersName;
+        this.flightNumber = flightNumber;
+        this.baggageWeight = baggageWeight;
+    }
+
+    public String getPassengersName() {
+        return passengersName;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public int getBaggageWeight() {
+        return baggageWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя пассажира: " + passengersName +
+               ", Номер рейса: " + flightNumber +
+               " ,Вес багажа:" + baggageWeight;
+    }
+
 }
+
